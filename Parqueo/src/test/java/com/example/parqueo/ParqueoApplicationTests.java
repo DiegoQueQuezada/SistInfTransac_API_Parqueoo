6bdd166077f39939d14a44fca321f026cda206df
Diego Quezada <diegodavid0404@hotmail.com>
2023-05-06 15:14:45 -0500

    Estructura del proyecto API_Parqueo "Please enter the commit message for your changes. Lines starting

---
+++ Parqueo/src/test/java/com/example/parqueo/ParqueoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parqueo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
