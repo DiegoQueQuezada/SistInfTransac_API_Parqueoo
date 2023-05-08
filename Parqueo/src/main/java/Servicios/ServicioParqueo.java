b22c73d3abb5d1693bef50c7604415f96931e592
Diego Quezada <diegodavid0404@hotmail.com>
2023-05-08 16:14:41 -0500

    feat: Crear la clase parkingService con las HU1 Y HU2

--- Parqueo/src/main/java/Servicios/ServicioParqueo.java
+++ Parqueo/src/main/java/Servicios/ServicioParqueo.java
@@ -1,4 +1,34 @@
 package Servicios;
+import Entidades.Carro;
+
+import java.util.ArrayList;
 
 public class ServicioParqueo {
+
+
+    private ArrayList<Carro> listaParqueos;
+
+
+
+    public ServicioParqueo(){
+        listaParqueos=null;
+    }
+    //TODO: HISTORA DE USUARIO 4
+    //Como usuario, quiero poder agregar un auto al parqueo, para poder estacionar
+    //mi vehículo.
+    public void agregarParqueo(Carro carro){
+        this.listaParqueos.add(carro);
+    }
+    //TODO: HISTORA DE USUARIO 1
+    //Como usuario, quiero poder ver la lista de todos los autos estacionados en el
+    //parqueo, para poder tener una visión general del estado actual del
+    //estacionamiento.
+
+    public ArrayList<Carro> getListaParqueos() {
+        return listaParqueos;
+    }
+
+    public void setListaParqueos(ArrayList<Carro> listaParqueos) {
+        this.listaParqueos = listaParqueos;
+    }
 }
