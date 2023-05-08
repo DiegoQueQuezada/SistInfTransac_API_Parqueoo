68bd97568024035c3e2e53f6b53514521dec0d80
Diego Quezada <diegodavid0404@hotmail.com>
2023-05-08 15:15:07 -0500

    feat: Crea la entidad Car

--- Parqueo/src/main/java/Entidades/Carro.java
+++ Parqueo/src/main/java/Entidades/Carro.java
@@ -1,4 +1,36 @@
 package Entidades;
 
+import java.time.LocalDateTime;
+
 public class Carro {
+
+    private String licencia;
+    private String color;
+    private LocalDateTime fechaTiempo;
+
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
+    public LocalDateTime getFechaTiempo() {
+        return fechaTiempo;
+    }
+
+    public void setFechaTiempo(LocalDateTime fechaTiempo) {
+        this.fechaTiempo = fechaTiempo;
+    }
 }
